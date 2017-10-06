2d215ec790f1d4361ad134dece7c1e8ea565c646
Gabriel Angelus <gangeluscf@gmail.com>
2017-10-05 22:24:51 -0300

    Respondendo todos automaticamente!

--- app.js
+++ app.js
@@ -1,43 +1,167 @@
 ;(function( doc ) {
 
-  const socket = new WebSocket('ws://34.212.9.57:5000');
-  
-  // Connection opened
-  socket.addEventListener('open', function (event) {
-    console.log('ws conectado');
-      // socket.send('Hello Server!');
-  });
-  
-  // Listen for messages
-  socket.addEventListener('message', function (event) {
-      console.log('Message from server ', event.data);
-  });
+  $(document).ready(function(){
 
-  alert('Ativando...');
+    function sleep(milliseconds) {
+      var start = new Date().getTime();
+      for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds){
+          break;
+        }
+      }
+    }
 
-  var sended = false
+  request = new XMLHttpRequest();  
+
+  var sending = false
 
   setInterval(
     function(){
+      console.log('Tentando localizar mensagens...')
 
-      if (!sended) {
+      if (!sending) {
+        if ($('.chat.unread').length > 0) {
+          sending = true
 
-        console.log('tentando localizar mensagens...')
+          $.ajax({
+            async: false,
+            url: 'https://condominiodedicado.com.br/bot/?' + $.param({
+              "type": "mouse-move",
+              "x": $('.chat.unread')[0].getBoundingClientRect().x + 200,
+              "y": $('.chat.unread')[0].getBoundingClientRect().y + 127,
+            }),
+            success: function() {
+              $.ajax({
+                async: false,
+                url: 'https://condominiodedicado.com.br/bot/?' + $.param({"type": "mouse-click"}),
+                success: function() {
+                  setTimeout(function(){
+                    $.ajax({
+                      async: false,
+                      url: 'https://condominiodedicado.com.br/bot/?' + $.param({
+                        "type": "mouse-move",
+                        "x": $('.pluggable-input-placeholder')[0].getBoundingClientRect().x + 40,
+                        "y": $('.pluggable-input-placeholder')[0].getBoundingClientRect().y + 87,
+                      }),
+                      success: function() {
+                        $.ajax({
+                          async: false,
+                          url: 'https://condominiodedicado.com.br/bot/?' + $.param({
+                            "type": "mouse-click",
+                          }),
+                          success: function() {
+                            $.ajax({
+                              async: false,
+                              url: 'https://condominiodedicado.com.br/bot/?' + $.param({
+                                "type": "keyboard-input",
+                                "text": "Ola, tudo bem?"
+                              }),
+                              success: function() {
+                                setTimeout(function(){
+                                  $.ajax({
+                                    async: false,
+                                    url: 'https://condominiodedicado.com.br/bot/?' + $.param({
+                                      "type": "mouse-move",
+                                      "x": $('[data-icon="send"]')[0].getBoundingClientRect().x + 20,
+                                      "y": $('[data-icon="send"]')[0].getBoundingClientRect().y + 100
+                                    }),
+                                    success: function() {                 
+                                      $.ajax({
+                                        async: false,
+                                        url: 'https://condominiodedicado.com.br/bot/?' + $.param({
+                                          "type": "mouse-click"
+                                        }),
+                                        success: function() {                 
+                                          $.ajax({
+                                            async: false,
+                                            url: 'https://condominiodedicado.com.br/bot/?' + $.param({
+                                              "type": "mouse-move",
+                                              "x": $('[data-icon="pinned"]').closest('.infinite-list-item')[0].getBoundingClientRect().x + 200,
+                                              "y": $('[data-icon="pinned"]').closest('.infinite-list-item')[0].getBoundingClientRect().y + 127,
+                                            }),
+                                            success: function() {                 
+                                              $.ajax({
+                                                async: false,
+                                                url: 'https://condominiodedicado.com.br/bot/?' + $.param({
+                                                  "type": "mouse-click",
+                                                }),
+                                                success: function() {                 
+                                                  sending = false
+                                                }
+                                              })
+                                            }
+                                          })
+                                        }
+                                      })
+                                    }
+                                  })
+                                }, 1200)                                
+                              }
+                            })                          
+                          }
+                        })
+                      }
+                    })
+                }, 1200);
+                 
+                }
+              })
+            }
+          })
 
-        if ($('[data-icon="pinned"]').length > 0) {
-          obj = {
-            "type": "mouse-move",
-            "x": $('[data-icon="pinned"]').closest('.infinite-list-item')[0].offsetWidth,
-            "y": $('[data-icon="pinned"]').closest('.infinite-list-item')[0].offsetHeight
-          }
-          socket.send(JSON.stringify(obj));
-          sended = true
         }
-
+      } else {
+        sleep(4000)
       }
+
+        /*if (!sending) {
+          
+          sending = true
+
+          $.get( 'https://condominiodedicado.com.br/bot/?' + $.param({
+            "type": "mouse-move",
+            "x": $('.chat.unread')[0].getBoundingClientRect().x + 200,
+            "y": $('.chat.unread')[0].getBoundingClientRect().y + 127,
+          }), () => {
+            $.get( 'https://condominiodedicado.com.br/bot/?' + $.param({
+              "type": "mouse-click"
+            }), () => {
+              $.get( 'https://condominiodedicado.com.br/bot/?' + $.param({
+                "type": "keyboard-input",
+                "text": "Olá!"
+              }), () => {
+                $.get( 'https://condominiodedicado.com.br/bot/?' + $.param({
+                  "type": "mouse-move",
+                  "x": $('[data-icon="send"]')[0].getBoundingClientRect().x + 20,
+                  "y": $('[data-icon="send"]')[0].getBoundingClientRect().y + 100
+                }), () => {
+                  $.get( 'https://condominiodedicado.com.br/bot/?' + $.param({
+                    "type": "mouse-click"
+                  }), () => {
+                    $.get( 'https://condominiodedicado.com.br/bot/?' + $.param({
+                      "type": "mouse-move",
+                      "x": $('[data-icon="pinned"]').closest('.infinite-list-item')[0].getBoundingClientRect().x + 200,
+                      "y": $('[data-icon="pinned"]').closest('.infinite-list-item')[0].getBoundingClientRect().y + 127,
+                    }), () => {
+                      $.get( 'https://condominiodedicado.com.br/bot/?' + $.param({
+                        "type": "mouse-click",
+                      }), function() {
+                        sending = false
+                      } )
+                    } )
+                  } )
+                } )
+              } )
+            } )
+          } )
+        } else {
+          sleep(4000);
+        }*/
 
     },
     3000
   );
+
+  })
   
   })( document );
--- emitter.js
+++ emitter.js
@@ -2,8 +2,8 @@ let events = [{"type": "mouse-move", "x":400, "y":350},
 {"type": "mouse-click"},
 {"type": "mouse-move", "x":900, "y":965},
 {"type": "mouse-click"},
-{"type": "keyboard-input", "text":"Oi, meu nome é Gabriel Angelus!"},
-{"type": "mouse-move", "x":1650, "y":965},
+{"type": "keyboard-input", "text":"Oi, tudo bem?"},
+{"type": "mouse-move", "x":1465, "y":935},
 {"type": "mouse-click"}]
 
 const sleep = require('sleep');
@@ -13,8 +13,10 @@ const WebSocket = require('ws');
 const ws = new WebSocket('ws://34.212.9.57:5000');
  
 ws.on('open', function open() {
-    console.log('conectou')
-  sleep.sleep(3)
+    console.log('Conectou.')
+    console.log('Emitindo eventos...')
+    sleep.sleep(3)
+
     for(let i=0; i<events.length; i++) {
         sleep.msleep(500)
         ws.send(JSON.stringify(events[i]))
